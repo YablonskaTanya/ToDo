a62e5d48be0079b8940ea7d3bcb41a8d9dbd057f
YablonskaTanya <111698588+YablonskaTanya@users.noreply.github.com>
2023-03-22 19:59:31 +0000

    Deploying to gh-pages from @ YablonskaTanya/ToDo@4bcbbf880ba82439f2f3ff97c61c50a59b96e58d 🚀

---
+++ index.1f328ec1.js
@@ -0,0 +1,2 @@
+document.querySelector("form"),document.querySelector(".task-list");
+//# sourceMappingURL=index.1f328ec1.js.map
---
+++ index.409b7f77.js
@@ -0,0 +1,2 @@
+document.querySelector("form"),document.querySelector(".task-list");
+//# sourceMappingURL=index.409b7f77.js.map
