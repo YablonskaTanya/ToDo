cd1b776d6ae1d98b65ab6278221b196b0d191619
YablonskaTanya <111698588+YablonskaTanya@users.noreply.github.com>
2023-03-30 18:26:04 +0300

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import { onDeleteTaskBtnClick, onFormSubmit, STORAGE_KEY } from './js/handlers';
 import { refs } from './js/refs';
+import { renderTaskItem } from './js/renderFunction';
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.taskList.addEventListener('click', onDeleteTaskBtnClick);
+
+renderTaskItem(
+  refs.taskList,
+  JSON.parse(localStorage.getItem(STORAGE_KEY)) || []
+);
